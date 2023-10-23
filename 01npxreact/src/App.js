80c0b39a5dde998c5ef92f954a40330b118a1dbd
Priyanka <sainipriyanka1722001@gmail.com>
2023-10-23 22:34:46 +0530

    two ways of creating react project

---
+++ 01npxreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Krishna aur Mai</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+ 
+  return (
+   <h1>Radhe Radhe</h1>
+  )
+}
+
+export default App
