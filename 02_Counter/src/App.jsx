13abcce0ed5483628deeb9fd50eb6b43be7dfd55
Priyanka <sainipriyanka1722001@gmail.com>
2023-10-24 15:06:14 +0530

    Learning Hooks(useState hooks) using Counter project

---
+++ 02_Counter/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  // let counter = 10;
+//hooks
+//useState hook will propogate and update the value of variable to all the UI elements where it is used.
+// syntax --  
+// let [variable , variableUpdationFunction] = useState(default value of variable);
+  let [counter, setCounter]= useState(10);
+
+  function addValue(){
+    // counter = counter+1;
+    if(counter === 20){
+      setCounter(counter);
+    }
+    else{
+      setCounter(counter+1);
+    }
+    
+  }
+
+  const reduceValue = ()=>{
+    // counter = counter -1;
+    if(counter === 0){
+      setCounter(counter);
+    }
+    else{
+      setCounter(counter-1);
+    }
+ 
+  }
+
+
+  return (
+    <>
+     <h1>Counter</h1>
+     <h2>Counter value : {counter} </h2>
+
+
+     <button
+     onClick={addValue}
+     >Counter Up</button>
+
+
+     <button
+     onClick={reduceValue}
+     >Counter Down</button>
+
+     <h2>New Counter: {counter}</h2>
+     {counter}
+    </>
+  )
+}
+
+export default App
