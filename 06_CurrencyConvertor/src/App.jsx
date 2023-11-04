215955b7a2b5c71ae0605f07c7f809b9abe822e2
Priyanka <sainipriyanka1722001@gmail.com>
2023-11-04 18:55:33 +0530

    component creation

--- 06_CurrencyConvertor/src/App.jsx
+++ 06_CurrencyConvertor/src/App.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
 import './App.css'
-
+import { InputBox } from './components'
+import useCurrencyInfo from "./hooks/useCurrencyInfo"
 function App() {
-  
+  const [amount,onAmountChange]= useState(0)
+  const [from,setFrom ]= useState("usd")
+  const [to,setTo ] = useState("inr")
+  const [convertedAmount, SetconvertedAmount] = useState(0)
 
+  const currencyInfo
   return (
     <>
       <h1 className='bg-black text-3xl text-white  p-8 rounded-xl'>Hare Krishna! Kridha💘💗💕💞 | Radha Krishna</h1>
