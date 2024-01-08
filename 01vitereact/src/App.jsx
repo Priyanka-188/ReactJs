f83271906cbdbe561e47e1101a3fb4b4dfdf235e
Priyanka <sainipriyanka1722001@gmail.com>
2024-01-08 20:08:43 +0530

    new component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,10 @@
+import Details from "./Details";
 
 function App() {
  const username = "Kridha";
   return (
     <>
+    <Details/>
    <h1>Radhe 💗</h1>
    <span>Thanks {username}</span>
    </>
---
+++ 01vitereact/src/Details.jsx
@@ -0,0 +1,10 @@
+function Details(){
+    return(
+        <>
+        <h1>Radha Krishna Mere Pran</h1>
+        <p>Vo mere mai Unki</p>
+        </>
+    )
+}
+
+export default Details;
